57e142dc6f5dfc6c6394d04ff4e807bb8288a417
Noah Loomans <noahloomans@gmail.com>
2016-09-06 15:59:57 +0200

    Fixing safari is imposible, so I used tools to do it for me.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 const gulp = require('gulp')
 const browserify = require('gulp-browserify')
+const babel = require('gulp-babel')
 
 // Basic usage
 gulp.task('scripts', function () {
   // Single entry point to browserify
-  gulp.src('public/javascripts/main.js')
+  return gulp.src('public/javascripts/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
     .pipe(browserify({
       insertGlobals: true,
       debug: !gulp.env.production
     }))
-    .pipe(gulp.dest('public/javascripts/bundle.js'))
+    .pipe(gulp.dest('public/javascripts/dest'))
 })
 
 gulp.task('scripts:watch', function () {
