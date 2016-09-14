c83f9a65ef1c207ee6ef503f55893fd26aa6d39f
Noah Loomans <noahloomans@gmail.com>
2016-09-14 18:50:31 +0200

    update package.json and change logtype

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-app.use(logger('dev'))
+app.use(logger('common'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
