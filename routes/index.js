026ec47c693d74307ba780b8e998f0196dd9f195
Noah Loomans <noahloomans@gmail.com>
2016-11-10 15:28:13 +0100

    added new style unit test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const users = require('../lib/getUserIndex')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { users: users.users })
+  if (req.query.v === 'n') {
+    res.render('index', { version: 'new', users: users.users })
+  } else {
+    res.render('index', { version: 'old', users: users.users })
+  }
 })
 
 module.exports = router
