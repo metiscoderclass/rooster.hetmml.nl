aacb48afce4ee021535f938b2b418a2e0796d105
Noah Loomans <noahloomans@gmail.com>
2017-03-22 12:55:15 +0100

    Fix sending wrong file when not in correct directory

--- routes/manifest.js
+++ routes/manifest.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
 router.get('/', function (req, res, next) {
   console.log('got a request')
@@ -9,9 +10,9 @@ router.get('/', function (req, res, next) {
   const isBeta = process.env.BETA === '1'
 
   if (isBeta) {
-    res.sendFile('manifest.beta.webmanifest', { root: './public' })
+    res.sendFile('manifest.beta.webmanifest', { root: path.join(__dirname, '../public') })
   } else {
-    res.sendFile('manifest.webmanifest', { root: './public' })
+    res.sendFile('manifest.webmanifest', { root: path.join(__dirname, '../public') })
   }
 })
 
