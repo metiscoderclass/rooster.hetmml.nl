65888ffdcbdd4d663360cd2faa15997b8a1f99c7
Noah Loomans <noahloomans@gmail.com>
2017-05-26 18:47:26 +0200

    Remove /hello page
    It not used anymore

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const compression = require('compression')
 const routes = require('./routes/index')
 const meetingpointProxy = require('./routes/meetingpointProxy')
 const manifest = require('./routes/manifest')
-const hello = require('./routes/hello')
 
 const app = express()
 
@@ -28,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', routes)
 app.use('/meetingpointProxy', meetingpointProxy)
-app.use('/hello', hello)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
