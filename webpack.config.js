edaa91d05e753e3609c00068f565b88c4ef77e62
Noah Loomans <noahloomans@gmail.com>
2017-12-10 11:37:32 +0100

    Compile scss to css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+const js = {
   entry: './src/client/react/index.jsx',
   output: {
     path: path.resolve(__dirname, 'src/client/static'),
@@ -18,5 +19,27 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
-  }
+  },
 };
+
+const style = {
+  entry: './src/client/style/index.scss',
+  output: {
+    path: path.resolve(__dirname, 'src/client/static'),
+    filename: 'bundle.css',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.scss$/],
+        exclude: [/node_modules/],
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+  ],
+};
+
+module.exports = [js, style];
