2216d1ceed02e54620f16fb826e5947b6c2cb9bf
Noah Loomans <noahloomans@gmail.com>
2017-12-11 20:43:07 +0100

    Add line between results and search

--- src/client/react/components/container/Results.jsx
+++ src/client/react/components/container/Results.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
 import Result from '../presentational/Result';
 
 const Results = (({ results }) => (
-  results.map(user => (
-    <Result key={user.value} user={user} />
-  ))
+  <div
+    className={classnames('search__results', {
+      'search__results--has-results': results.length > 0,
+    })}
+  >
+    {results.map(user => (
+      <Result key={user.value} user={user} />
+    ))}
+  </div>
 ));
+
+Results.propTypes = {
+  results: PropTypes.arrayOf(PropTypes.shape({
+    type: PropTypes.string,
+    value: PropTypes.value,
+  })).isRequired,
+};
 
 const mapStateToProps = state => ({
   results: state.search.results,
