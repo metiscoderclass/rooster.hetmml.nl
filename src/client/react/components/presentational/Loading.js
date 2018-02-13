532d262f53de71eaf1718ab9668c404f635dffdf
Noah Loomans <noahloomans@gmail.com>
2018-02-13 13:54:54 +0100

    Add proper progress bar

--- src/client/react/components/presentational/Loading.js
+++ src/client/react/components/presentational/Loading.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { LinearProgress } from 'rmwc/LinearProgress';
 
 class Loading extends React.Component {
   render() {
-    return <div>Loading...</div>;
+    return (
+      <LinearProgress determinate={false} />
+    );
   }
 }
 
