7250c51f716a050c105aae4e9f46d9736e108654
Noah Loomans <noahloomans@gmail.com>
2018-02-17 19:33:57 +0100

    Change LICENSE to GPLv3

--- src/client/react/components/presentational/Loading.js
+++ src/client/react/components/presentational/Loading.js
@@ -1,3 +1,23 @@
+/**
+ * Copyright (C) 2018 Noah Loomans
+ *
+ * This file is part of rooster.hetmml.nl.
+ *
+ * rooster.hetmml.nl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rooster.hetmml.nl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rooster.hetmml.nl.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import React from 'react';
 import { LinearProgress } from 'rmwc/LinearProgress';
 
