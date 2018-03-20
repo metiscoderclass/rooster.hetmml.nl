3cb7c15d15f33e9b0ce06dbf0072021f6921dbe7
Noah Loomans <noahloomans@gmail.com>
2018-03-20 16:02:22 +0100

    Rewrite users.js without using redux

--- src/client/react/users.js
+++ src/client/react/users.js
@@ -20,68 +20,21 @@
 
 /* global USERS */
 
-import { combineReducers, createStore } from 'redux';
+import keyBy from 'lodash/keyBy';
 
 const getId = ({ type, value }) => `${type}/${value}`;
 
-const byId = (state = {}, action) => {
-  switch (action.type) {
-    case 'USERS/ADD_USER':
-      return {
-        ...state,
-        [action.user.id]: {
-          ...action.user,
-        },
-      };
-    default:
-      return state;
-  }
-};
+const users = {};
 
-const allIds = (state = [], action) => {
-  switch (action.type) {
-    case 'USERS/ADD_USER':
-      return [
-        ...state,
-        action.user.id,
-      ];
-    default:
-      return state;
-  }
-};
-
-const allUsers = (state = [], action) => {
-  switch (action.type) {
-    case 'USERS/ADD_USER':
-      return [
-        ...state,
-        {
-          ...action.user,
-        },
-      ];
-    default:
-      return state;
-  }
-};
-
-const store = createStore(combineReducers({
-  byId,
-  allIds,
-  allUsers,
+users.allUsers = USERS.map(user => ({
+  type: user.type,
+  value: user.value,
+  alt: user.alt,
+  id: getId(user),
 }));
 
-USERS.forEach((user) => {
-  store.dispatch({
-    type: 'USERS/ADD_USER',
-    user: {
-      type: user.type,
-      value: user.value,
-      alt: user.alt,
-      id: getId(user),
-    },
-  });
-});
+users.allIds = users.allUsers.map(user => user.id);
 
-const users = store.getState();
+users.byId = keyBy(users.allUsers, 'id');
 
 export default users;
