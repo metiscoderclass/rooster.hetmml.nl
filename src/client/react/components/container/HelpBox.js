2e426cce0f3649cbcc21717e79b18434a8a66f35
Noah Loomans <noahloomans@gmail.com>
2018-03-21 19:10:31 +0100

    Migrate HelpBox style

--- src/client/react/components/container/HelpBox.js
+++ src/client/react/components/container/HelpBox.js
@@ -22,6 +22,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import './HelpBox.scss';
+
 class HelpBox extends React.Component {
   static propTypes = {
     // redux
@@ -35,7 +37,7 @@ class HelpBox extends React.Component {
     }
 
     return (
-      <div className="help-box">
+      <div className="HelpBox">
         <div className="arrow" />
         <div className="bubble">
           Voer hier een docentafkorting, klas, leerlingnummer of lokaalnummer in.
