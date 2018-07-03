5451dc2f5a09fa06df32d1a1661623b475170566
Noah Loomans <noahloomans@gmail.com>
2018-07-03 19:33:51 +0200

    client: Document getHistory

--- src/client/react/lib/getHistory.js
+++ src/client/react/lib/getHistory.js
@@ -5,7 +5,20 @@ import {
   makeUpdateQuery,
 } from './url';
 
+/**
+ * Make a getHistory function. This function is used in index.js when creating
+ * the redux store.
+ * @param {history} history
+ *   The history object from the `history` package.
+ *   There may only be a single shared history object in the application, which
+ *   is why it's delivered from `../index.js`.
+ */
 export default function makeGetHistory(history) {
+  /**
+   * Get a collection of helpers for common browser history interactions, and a
+   * collection of precalculated values from the address bar. This function is
+   * used in actions.
+   */
   return function getHistory() {
     const user = userFromLocation(history.location);
     const week = weekFromLocation(history.location);
